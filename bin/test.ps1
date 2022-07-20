92770193f80d3af8c9b624678f024e37c5955b88
Sven Pfennig <s.pfennig@reply.de>
2022-07-20 12:52:55 +0200

    [CI] Verify proof solutions for exercises on windows
    Signed-off-by: Sven Pfennig <s.pfennig@reply.de>

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,10 +1,29 @@
+function verify_exercise ($practiceExerciseDir) {
+    $dir=[System.IO.Path]::GetFullPath($practiceExerciseDir)
+    $slug=Split-Path $dir -Leaf
+    $implementationFile="$dir/.meta/proof.ci.CBL"
+    $stubFile="$dir/src/$slug.CBL"
+    $stubBackupFile="$stubFile.bak"
+    Copy-Item $stubFile -Destination $stubBackupFile
+    Copy-Item $implementationFile -Destination $stubFile
+
+    Invoke-Expression "$dir/test.ps1"
+
+    if ($Lastexitcode -ne 0) {
+        Write-Output $slug": proof solution did not pass the tests"
+        exit 1
+    }
+
+    Copy-Item $stubBackupFile -Destination $stubFile
+}
+
 Get-ChildItem -Directory ./exercises/concept/ |
     ForEach-Object {
         Write-Output "Checking $_ exercise..."$_
-        & "$_/test.ps1"
+        verify_exercise $_
     }
 Get-ChildItem -Directory ./exercises/practice/ |
     ForEach-Object {
         Write-Output "Checking $_ exercise..."
-        & "$_/test.ps1"
+        verify_exercise $_
     }
