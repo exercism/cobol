7f70b5b0ff55428545e27e387e15cb8bd926747b
Bruce M. Axtens <bugmagnet@outlook.com>
2022-07-20 14:53:34 +0800

    test.ps1 fixes

--- exercises/practice/hello-world/test.ps1
+++ exercises/practice/hello-world/test.ps1
@@ -1,14 +1,15 @@
 if (![System.IO.File]::Exists("$PSScriptRoot/bin/cobolcheck.exe")){
-  Write-Output "Cobolcheck not found, try to fetch it."
+  Write-Output "Cobolcheck not found. Trying to fetch it."
   & "$PSScriptRoot/bin/fetch-cobolcheck.ps1"
 }
 
 Write-Output "Run cobolcheck."
-cd $PSScriptRoot
+Set-Location $PSScriptRoot
 
 
-Invoke-Expression "bin/cobolcheck.exe -p hello-world -l debug && cobc -x TEST.CBL && TEST.exe"
-    
+Invoke-Expression "bin/cobolcheck.exe -p hello-world"
+Invoke-Expression "cobc -xj TEST.CBL"
+
 if ($Lastexitcode -ne 0) {
   exit $Lastexitcode
 }
