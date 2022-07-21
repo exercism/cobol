cdc0ad489b40e235b6faf799327de49dea29cb92
Bruce M. Axtens <bugmagnet@outlook.com>
2022-07-21 21:13:54 +0800

    adding leap

---
+++ exercises/practice/leap/src/leap.cbl
@@ -0,0 +1,11 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. LEAP.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       LEAP.
+	  * Enter solution here
+		 CONTINUE.
+       LEAP-EXIT.
+         EXIT.
---
+++ exercises/practice/leap/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/
+
+if [ ! -f $SCRIPT_DIR/bin/cobolcheck ]; then
+    echo "Cobolcheck not found, try to fetch it."
+    cd $SCRIPT_DIR/bin/
+    ./fetch-cobolcheck
+fi
+cd $SCRIPT_DIR
+./bin/cobolcheck -p leap
+cobc -x TEST.CBL
+./TEST
