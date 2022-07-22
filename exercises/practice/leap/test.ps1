5c623217a7c235d7f8c0c910c822f8b2d694e39c
Bruce Axtens <bruce.axtens@gmail.com>
2022-07-22 16:42:40 +0800

    Update exercises/practice/leap/test.ps1
    Co-authored-by: Sven Pfennig <git@sven.pfennig.it>

--- exercises/practice/leap/test.ps1
+++ exercises/practice/leap/test.ps1
@@ -1,5 +1,6 @@
 if (![System.IO.File]::Exists("$PSScriptRoot/bin/cobolcheck.exe")){
-  Write-Output "Cobolcheck not found. Trying to fetch it."  & "$PSScriptRoot/bin/fetch-cobolcheck.ps1"
+  Write-Output "Cobolcheck not found. Trying to fetch it."  
+  & "$PSScriptRoot/bin/fetch-cobolcheck.ps1"
 }
 
 Write-Output "Run cobolcheck."
