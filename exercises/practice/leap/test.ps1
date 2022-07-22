f800240e681f73b387f6a070b27b47c0f4ca5fbf
Bruce Axtens <bruce.axtens@gmail.com>
2022-07-22 21:33:33 +0800

    backslash?

--- exercises/practice/leap/test.ps1
+++ exercises/practice/leap/test.ps1
@@ -1,4 +1,4 @@
-if (![System.IO.File]::Exists("$PSScriptRoot/bin/cobolcheck.exe")){
+if (![System.IO.File]::Exists("$PSScriptRoot\bin\cobolcheck.exe")){
   Write-Output "Cobolcheck not found. Trying to fetch it."  
   & "$PSScriptRoot/bin/fetch-cobolcheck.ps1"
 }
@@ -7,7 +7,7 @@ Write-Output "Run cobolcheck."
 Set-Location $PSScriptRoot
 
 
-Invoke-Expression "bin/cobolcheck.exe -p leap"
+Invoke-Expression "bin\cobolcheck.exe -p leap"
 Invoke-Expression "cobc -xj test.cob"
 
 if ($Lastexitcode -ne 0) {
