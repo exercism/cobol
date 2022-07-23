563ccf5fe4e7a1e6a6170c1e872a7de96e7e0a4a
Bruce Axtens <bruce.axtens@gmail.com>
2022-07-24 00:03:40 +0800

    [Feature]: Added isogram exercise
    * initial
    
    * whoopsy
    
    * bits
    
    * backslashes?
    
    * backslashes?
    
    * Update config.json
    
    Co-authored-by: Keith Taylor <73310001+KTSnowy@users.noreply.github.com>

--- exercises/practice/isogram/test.ps1
+++ exercises/practice/isogram/test.ps1
@@ -1,12 +1,13 @@
 if (![System.IO.File]::Exists("$PSScriptRoot\bin\cobolcheck.exe")){
-  Write-Output "Cobolcheck not found. Trying to fetch it."
+  Write-Output "Cobolcheck not found. Trying to fetch it."  
   & "$PSScriptRoot\bin\fetch-cobolcheck.ps1"
 }
 
 Write-Output "Run cobolcheck."
 Set-Location $PSScriptRoot
 
-Invoke-Expression "bin\cobolcheck.exe -p hello-world"
+
+Invoke-Expression "bin\cobolcheck.exe -p isogram"
 Invoke-Expression "cobc -xj test.cob"
 
 if ($Lastexitcode -ne 0) {
