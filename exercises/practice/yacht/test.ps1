b3b7d5d29c71afb5899fb22a9198f3a61b4b3389
Bruce Axtens <bruce.axtens@gmail.com>
2022-07-23 20:24:03 +0800

    Yacht too (#26)
    * second attempt on yacht
    
    * bits
    
    * passing more test cases
    
    * one left
    
    * all passing

---
+++ exercises/practice/yacht/test.ps1
@@ -0,0 +1,14 @@
+if (![System.IO.File]::Exists("$PSScriptRoot\bin\cobolcheck.exe")){
+  Write-Output "Cobolcheck not found. Trying to fetch it."
+  & "$PSScriptRoot/bin/fetch-cobolcheck.ps1"
+}
+
+Write-Output "Run cobolcheck."
+Set-Location $PSScriptRoot
+
+Invoke-Expression "bin\cobolcheck.exe -p yacht"
+Invoke-Expression "cobc -xj test.cob"
+
+if ($Lastexitcode -ne 0) {
+  exit $Lastexitcode
+}
