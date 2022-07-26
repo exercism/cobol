56486a25d446ccd6cea153da83633514032fbb01
Bruce Axtens <bruce.axtens@gmail.com>
2022-07-26 13:19:11 +0800

    bob initial

---
+++ exercises/practice/bob/src/bob.cob
@@ -0,0 +1,8 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. BOB.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-HEYBOB                PIC X(60).
+       01 WS-RESULT                PIC X(40).
+       BOB.
