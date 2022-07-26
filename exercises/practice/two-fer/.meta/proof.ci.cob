18b500400417773f043b4dc5ede30bfc04c9bae2
KTSnowy <KTSnowy@outlook.com>
2022-07-25 22:09:48 -0300

    [Feature]: Added Two-Fer Exercise

---
+++ exercises/practice/two-fer/.meta/proof.ci.cob
@@ -0,0 +1,16 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. two-fer.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-NAME PIC X(16).
+       01 WS-RESULT PIC X(16).
+       
+       PROCEDURE DIVISION.
+       TWO-FER.
+
+       IF WS-NAME EQUAL SPACE THEN
+           MOVE "One for you, one for me." TO WS-RESULT
+       ELSE
+           MOVE "One for " FUNCTION TRIM(WS-NAME TRAILING)  
+              ", one for me." TO WS-RESULT
+       END-IF.
---
+++ exercises/practice/two-fer/src/two-fer.cob
@@ -0,0 +1,9 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. two-fer.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-NAME PIC X(16).
+       01 WS-RESULT PIC X(16).
+       
+       PROCEDURE DIVISION.
+       TWO-FER.
