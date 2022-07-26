a4bf08dbcb185f6d470c614e209fd5ae7b34e93f
Keith Taylor <73310001+KTSnowy@users.noreply.github.com>
2022-07-26 08:58:47 -0300

    Update proof.ci.cob

--- exercises/practice/two-fer/.meta/proof.ci.cob
+++ exercises/practice/two-fer/.meta/proof.ci.cob
@@ -11,7 +11,8 @@
        IF WS-NAME EQUAL SPACE THEN
            MOVE "One for you, one for me." TO WS-RESULT
        ELSE
-           MOVE FUNCTION CONCAT("One for ", 
-           FUNCTION TRIM(WS-NAME TRAILING),", one for me.") 
-           TO WS-RESULT
+           STRING "One for "
+           FUNCTION TRIM(WS-NAME TRAILING) 
+           ", one for me.")
+           INTO WS-RESULT
        END-IF.
