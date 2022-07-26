489655d5e07dbb7ab02d1e08482d14a23f9dabb4
Keith Taylor <73310001+KTSnowy@users.noreply.github.com>
2022-07-25 21:04:16 -0300

    [Feature]: Added Difference of Squares exercise (#36)
    * [Feature]: Added Difference of Squares exercise
    
    * [Fix]: Fix Difference of Squares tests

---
+++ exercises/practice/difference-of-squares/src/difference-of-squares.cob
@@ -0,0 +1,20 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. difference-of-squares.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-DIFFERENCE-OF-SQUARES PIC 9(8).
+       01 WS-SUM-OF-SQUARES PIC 9(8).
+       01 WS-SQUARE-OF-SUM PIC 9(8).
+       01 WS-NUMBER PIC 9(8).
+
+       PROCEDURE DIVISION.
+       
+       SQUARE-OF-SUM.
+
+       
+       SUM-OF-SQUARES.
+
+       
+       DIFFERENCE-OF-SQUARES.
+
+
