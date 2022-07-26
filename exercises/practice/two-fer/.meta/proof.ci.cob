2a334ba515d5fe652d2fafb673fad64645614321
Keith Taylor <73310001+KTSnowy@users.noreply.github.com>
2022-07-26 09:06:49 -0300

    [Feature]: Added Two-Fer Exercise (#39)
    * [Feature]: Added Two-Fer Exercise
    
    * [Fix]: Fixed intrinsic function calls
    
    * [Fix]: Fixed WS-RESULT picture size
    
    * Update proof.ci.cob
    
    * Update proof.ci.cob

---
+++ exercises/practice/two-fer/.meta/proof.ci.cob
@@ -0,0 +1,18 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. two-fer.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-NAME PIC X(16).
+       01 WS-RESULT PIC X(64).
+       
+       PROCEDURE DIVISION.
+       TWO-FER.
+
+       IF WS-NAME EQUAL SPACE THEN
+           MOVE "One for you, one for me." TO WS-RESULT
+       ELSE
+           STRING "One for "
+           FUNCTION TRIM(WS-NAME TRAILING) 
+           ", one for me."
+           INTO WS-RESULT
+       END-IF.
