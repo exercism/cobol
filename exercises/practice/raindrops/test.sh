8d9f74f7bc0d8c6f4be27fc8c334b8bf856812cb
Sven Pfennig <s.pfennig@reply.de>
2022-07-27 10:13:26 +0200

    [TEST-RUNNER]: Adjust test scripts for test runner and fix #45
    Signed-off-by: Sven Pfennig <s.pfennig@reply.de>

--- exercises/practice/raindrops/test.sh
+++ exercises/practice/raindrops/test.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/
-SLUG=$(basename "${SCRIPT_DIR}")
+SLUG=${1:-$(basename "${SCRIPT_DIR}")}
 
 if [ ! -f $SCRIPT_DIR/bin/cobolcheck ]; then
     echo "Cobolcheck not found, try to fetch it."
     cd $SCRIPT_DIR/bin/
-    ./fetch-cobolcheck
+    bash fetch-cobolcheck
 fi
 cd $SCRIPT_DIR
 ./bin/cobolcheck -p $SLUG
