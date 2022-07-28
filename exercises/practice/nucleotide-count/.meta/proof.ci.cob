0092b8bb2e919753764464c0ad8c0e4fabc7c349
Keith Taylor <73310001+KTSnowy@users.noreply.github.com>
2022-07-28 13:02:01 -0300

    [Feature]: Added Nucleotide Count exercise (#66)
    * [Feature]: Added Nucleotide Count exercise

---
+++ exercises/practice/nucleotide-count/.meta/proof.ci.cob
@@ -0,0 +1,41 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. nucleotide-count.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       REPOSITORY. FUNCTION ALL INTRINSIC.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-DNA PIC X(128).
+       01 WS-INDEX PIC 9(8).
+       01 WS-ERROR PIC X(36).
+       01 WS-A PIC 9(4).
+       01 WS-C PIC 9(4).
+       01 WS-G PIC 9(4).
+       01 WS-T PIC 9(4).
+
+       PROCEDURE DIVISION.
+       NUCLEOTIDE-COUNT.
+
+       PERFORM VARYING WS-INDEX FROM 1 BY 1 
+           UNTIL WS-INDEX > LENGTH(TRIM(WS-DNA))
+           
+           EVALUATE WS-DNA (WS-INDEX:1)
+           WHEN "A"
+               ADD 1 TO WS-A
+           WHEN "C"
+               ADD 1 TO WS-C
+           WHEN "G"
+               ADD 1 TO WS-G
+           WHEN "T"
+               ADD 1 TO WS-T
+           WHEN OTHER
+               MOVE "ERROR: Invalid nucleotide in strand" 
+               TO WS-ERROR
+           END-EVALUATE
+       END-PERFORM.
+
+       DISPLAY "A: " WS-A.
+       DISPLAY "C: " WS-C.
+       DISPLAY "G: " WS-G.
+       DISPLAY "T: " WS-T.
+       DISPLAY WS-ERROR.
