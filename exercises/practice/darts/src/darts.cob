8cb89db33646d281d8d0b6209dcb2c9c3a7ddcac
Sven Pfennig <s.pfennig@reply.de>
2022-07-28 16:15:23 +0200

    Update darts.cob

--- exercises/practice/darts/src/darts.cob
+++ exercises/practice/darts/src/darts.cob
@@ -3,8 +3,8 @@
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 WS-X PIC 9.
-       01 WS-Y PIC 9.
+       01 WS-X PIC 9V9.
+       01 WS-Y PIC 9V9.
        01 WS-RESULT PIC 99.
        PROCEDURE DIVISION.
        DARTS.
