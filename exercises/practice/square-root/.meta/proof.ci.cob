01327cd5803b4a897b298f341d4a4af7f8298f82
Keith Taylor <73310001+KTSnowy@users.noreply.github.com>
2022-07-28 01:46:12 -0300

    [Feature]: Added Square Root exercise (#60)
    * [Feature]: Added Square Root exercise
    
    * [Update]: Update the CI Proof to not use SQRT Function

---
+++ exercises/practice/square-root/.meta/proof.ci.cob
@@ -0,0 +1,12 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. square-root.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-NUMBER PIC 9(32).
+       01 WS-SQRT PIC 9(32).
+
+       PROCEDURE DIVISION.
+       SQUARE-ROOT.
+       COMPUTE WS-SQRT = WS-NUMBER ** .5 
+       
+       DISPLAY WS-SQRT.
---
+++ exercises/practice/square-root/src/square-root.cob
@@ -0,0 +1,10 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. square-root.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-NUMBER PIC 9(32).
+       01 WS-SQRT PIC 9(32).
+
+       PROCEDURE DIVISION.
+       SQUARE-ROOT.
+
