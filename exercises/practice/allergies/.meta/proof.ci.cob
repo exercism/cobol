2a502718f5667d3699c0d986b169d2eb59562cce
Glenn Jackman <glenn.jackman@gmail.com>
2023-08-01 08:52:47 -0400

    add exercise: allergies (#97)
    * add exercise: allergies
    
    * All variable names upper-cased.
    Rework the initialization of the Allergens table according to "Beginning COBOL for Programmers", ch 11.
    
    * use 88 condition vars

---
+++ exercises/practice/allergies/.meta/proof.ci.cob
@@ -0,0 +1,58 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. allergies.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-SCORE       PIC 999.
+       01 WS-ITEM        PIC X(12).
+       01 WS-RESULT      PIC A.
+           88 IS-ALLERGIC   VALUE "Y".
+           88 NOT-ALLERGIC  VALUE "N".
+       01 WS-RESULT-LIST PIC X(108).
+
+       01 ALLERGENS.
+           02 ALLERGEN-NAMES.
+               03 FILLER    PIC X(24) VALUE "eggs        peanuts".
+               03 FILLER    PIC X(24) VALUE "shellfish   strawberries".
+               03 FILLER    PIC X(24) VALUE "tomatoes    chocolate".
+               03 FILLER    PIC X(24) VALUE "pollen      cats".
+           02 FILLER REDEFINES ALLERGEN-NAMES.
+               03 ALLERGEN  PIC X(12) OCCURS 8 TIMES
+                            INDEXED BY INDEX-1 INDEX-2.
+
+       01 ALLERGEN-VALUE PIC 999.
+       01 SHIFTED        PIC 999.
+       01 LIST-SEPARATOR PIC X.
+
+       PROCEDURE DIVISION.
+       ALLERGIC-TO.
+           SET NOT-ALLERGIC TO TRUE.
+           PERFORM VARYING INDEX-1 FROM 1 BY 1 UNTIL index-1 > 8
+               IF ALLERGEN(INDEX-1) EQUAL TO WS-ITEM THEN
+      *            cobol lacks bitwise operators:
+      *                if (score >> (idx - 1)) & 1 == 1
+                   COMPUTE ALLERGEN-VALUE = 2 ** (INDEX-1 - 1)
+                   DIVIDE WS-SCORE BY ALLERGEN-VALUE GIVING SHIFTED
+                   IF FUNCTION REM(SHIFTED; 2) EQUAL TO 1 THEN
+                       SET IS-ALLERGIC TO TRUE
+                       EXIT PERFORM
+                   END-IF
+               END-IF
+           END-PERFORM.
+
+       LIST-ALLERGENS.
+           MOVE SPACES TO WS-RESULT-LIST
+           MOVE SPACE TO LIST-SEPARATOR
+           PERFORM VARYING INDEX-2 FROM 1 BY 1 UNTIL index-2 > 8
+               MOVE ALLERGEN(INDEX-2) TO WS-ITEM
+               PERFORM ALLERGIC-TO
+               IF IS-ALLERGIC THEN
+                   STRING
+                       WS-RESULT-LIST DELIMITED BY SPACE
+                       LIST-SEPARATOR DELIMITED BY SPACE
+                       ALLERGEN(INDEX-2) DELIMITED BY SPACE
+                       INTO WS-RESULT-LIST
+                   END-STRING
+                   MOVE "," TO LIST-SEPARATOR
+               END-IF
+           END-PERFORM.
