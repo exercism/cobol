a0226da6b865a48c79bb1d646e1150ac54a872b0
Erik Schierboom <erik_schierboom@hotmail.com>
2024-04-19 11:31:07 +0200

    Streamline

--- bin/setup-environment.ps1
+++ bin/setup-environment.ps1
@@ -1,11 +1,4 @@
-Get-Command "cobc" -ErrorAction SilentlyContinue
-Get-Command "cobc.exe" -ErrorAction SilentlyContinue
-ls ${env:GITHUB_WORKSPACE}
-ls ${env:GITHUB_WORKSPACE}\bin
-ls ${env:GITHUB_WORKSPACE}\gnucobol
-ls ${env:GITHUB_WORKSPACE}\gnucobol\bin
 $env:Path = "${env:GITHUB_WORKSPACE}\gnucobol\bin;${env:GITHUB_WORKSPACE}\bin;${env:Path}"
-printenv
 [System.Environment]::SetEnvironmentVariable('COB_CONFIG_DIR', "${gnucobolDir}\config")
 [System.Environment]::SetEnvironmentVariable('COB_COPY_DIR', "${gnucobolDir}\copy")
 [System.Environment]::SetEnvironmentVariable('COB_CFLAGS', "-I ${gnucobolDir}\include")
