bb597beb5a1cfaf66626891a13e71c75997544db
kapitaali <tepposaari@hotmail.com>
2024-04-25 12:28:26 +0300

    adding meetup

---
+++ exercises/practice/meetup/src/meetup.cob
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. MEETUP.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-YEAR                  PIC 9999.
+       01 WS-MONTH                 PIC 99.
+       01 WS-WEEK                  PIC X(10).
+       01 WS-DAYOFWEEK             PIC X(10).
+       01 WS-RESULT                PIC X(40).
+
+       PROCEDURE DIVISION. 
+
+       MEETUP.
