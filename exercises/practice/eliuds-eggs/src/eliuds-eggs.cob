b9e3c8141a4508a63b7d24f95be815f9aa082180
kapitaali <tepposaari@hotmail.com>
2024-05-27 11:47:54 +0300

    adding eliuds-eggs

---
+++ exercises/practice/eliuds-eggs/src/eliuds-eggs.cob
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. ELIUDS-EGGS.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-INPUTVARS.
+           05 WS-INPUT             PIC 9(10).
+       01 WS-OUTPUTVARS.
+           05 WS-RESULT            PIC 9999.
+       
+       PROCEDURE DIVISION.
+
+       EGG-COUNT.
+
