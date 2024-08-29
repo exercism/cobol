c1968128b19c883166a5101c3e64137191cf00ad
SimaDovakin <simadovakin999@gmail.com>
2024-08-29 16:41:16 +0300

    Attempt to fix the problem with tests in the web editor.

--- exercises/practice/matching-brackets/test.ps1
+++ exercises/practice/matching-brackets/test.ps1
@@ -1,7 +1,11 @@
 $slug=Split-Path $PSScriptRoot -Leaf
 $cobolcheck = Join-Path -Path "${HOME}" "cobolcheck" "cobolcheck.exe"
+$cobolcheckCmd = Get-Command "cobolcheck.exe" -ErrorAction SilentlyContinue
 
-if (![System.IO.File]::Exists("$cobolcheck")){
+if ($cobolcheckCmd) {
+  $cobolcheck = $cobolcheckCmd.Path
+  Write-Output "Found cobolcheck, using $cobolcheck"
+} elseif (![System.IO.File]::Exists("$cobolcheck")){
   $fetchCobolcheckPath = Join-Path -Path "${PSScriptRoot}" "bin" "fetch-cobolcheck.ps1"
   echo $fetchCobolcheckPath
   Write-Output "Cobolcheck not found. Trying to fetch it."
