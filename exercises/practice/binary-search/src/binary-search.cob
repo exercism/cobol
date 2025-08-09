4e993bb0f0c487a606742edad9e3570df00cb46b
Shinichi Sakata <shinichi.sakata@gmail.com>
2025-08-09 06:42:46 -0700

    Binary Search: Resolve the error about WS-ARRAY.
    - While the test assumes that WS-ARRAY is a string, the template cobol
    code delcared it as an integer array. The revised template now
    declares WS-ARRAY as a string.

--- exercises/practice/binary-search/src/binary-search.cob
+++ exercises/practice/binary-search/src/binary-search.cob
@@ -7,9 +7,7 @@
        01 WS-RESULT                PIC 99.
        01 WS-ERROR                 PIC X(40).
        01 WS-COUNT                 PIC 99.       
-       01 WS-ARRAY-TABLE. 
-         02 WS-ARRAY               OCCURS 1 TO 20 DEPENDING ON WS-COUNT.
-            05 ROWELEM             PIC 9(4).       
+       01 WS-ARRAY                 PIC X(60).
 
        PROCEDURE DIVISION.
        
